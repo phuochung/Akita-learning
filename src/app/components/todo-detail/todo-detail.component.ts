db9693fe3b801ed1c51455e51e6024685058be66
Nguyen Phuoc Hung <phuochung.nguyen@tanchonggroup.com>
2019-10-26 09:14:51 +0700

    Update import todosService file

--- src/app/components/todo-detail/todo-detail.component.ts
+++ src/app/components/todo-detail/todo-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
-import { TodosService } from 'src/app/services/todos.service';
+import { TodosService } from '../../services/todos.service';
 declare var $: any;
 
 @Component({
